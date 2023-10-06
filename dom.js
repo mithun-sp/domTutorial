9c9b75a1de86508cd917c7d15312540cce82d406
Mithun S <mithunpillai007@gmail.com>
2023-10-06 13:23:41 +0530

    Added querySelectorAll

--- dom.js
+++ dom.js
@@ -1,20 +1,35 @@
-let headerTitle = document.getElementById("header-title")
-let mainHeader = document.getElementById("main-header")
-let addItem = document.getElementById("add-item")
+// let headerTitle = document.getElementById("header-title")
+// let mainHeader = document.getElementById("main-header")
+// let addItem = document.getElementById("add-item")
 
-headerTitle.innerHTML = "<h3>Hello</h3>"
+// headerTitle.innerHTML = "<h3>Hello</h3>"
 
-mainHeader.style.borderBottom = "solid 3px #000"
+// mainHeader.style.borderBottom = "solid 3px #000"
 
-addItem.style.fontWeight= "bold"
-addItem.style.color = "green"
+// addItem.style.fontWeight= "bold"
+// addItem.style.color = "green"
 
-let items = document.getElementsByClassName("list-group-item")
-items[2].style.backgroundColor = "green"
+// let items = document.getElementsByClassName("list-group-item")
+// items[2].style.backgroundColor = "green"
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight = "bold"
-}
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight = "bold"
+// }
 
-let li = document.getElementsByTagName("li")
-li[4].innerText = "Hello 5"
+// let li = document.getElementsByTagName("li")
+// li[4].innerText = "Hello 5"
+
+//Query Selector
+let secondItem = document.querySelector(".list-group-item:nth-child(2)")
+secondItem.style.backgroundColor = "green"
+
+let thirdItem = document.querySelector(".list-group-item:nth-child(3)")
+thirdItem.style.display = "none"
+
+//Query Selectore All
+let titles = document.querySelectorAll(".title")
+console.log(titles)
+
+let odd = document.querySelectorAll(".li:nth-child(odd)")
+let even = document.querySelectorAll(".li:nth-child(even)")
+
