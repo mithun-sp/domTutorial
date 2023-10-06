bd3bf7044c3c5a77d0b5add3deb691172ef4ceab
Mithun S <mithunpillai007@gmail.com>
2023-10-06 21:36:18 +0530

    Added description

--- dom.js
+++ dom.js
@@ -14,7 +14,12 @@ function addItem(e){
   e.preventDefault();
 
   // Get input value
-  var newItem = document.getElementById('item').value;
+  var newItem = document.getElementById('title').value;
+  var newItemDescription = document.getElementById('description').value;
+
+  let desc = document.createElement("span")
+  desc.className = "ml-4"
+  desc.innerText = newItemDescription 
 
   // Create new li element
   var li = document.createElement('li');
@@ -22,21 +27,30 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(desc);
 
   // Create del button element
   var deleteBtn = document.createElement('button');
+  let editBtn = document.createElement("button")
 
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  editBtn.className = "btn btn-sm float-right mr-2"
+  
 
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode("EDIT"))
 
   // Append button to li
   li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
 
   // Append li to list
   itemList.appendChild(li);
+  document.getElementById('title').value = ""
+  document.getElementById('description').value = ""
+  
 }
 
 // Remove item
@@ -57,7 +71,7 @@ function filterItems(e){
   var items = itemList.getElementsByTagName('li');
   // Convert to an array
   Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
+    var itemName = item.textContent;
     if(itemName.toLowerCase().indexOf(text) != -1){
       item.style.display = 'block';
     } else {
