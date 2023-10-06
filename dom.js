a65f46178c18960835c2371ee77569c28124c4e7
Mithun S <mithunpillai007@gmail.com>
2023-10-06 12:43:43 +0530

    First commit

---
+++ dom.js
@@ -0,0 +1,10 @@
+let headerTitle = document.getElementById("header-title")
+let mainHeader = document.getElementById("main-header")
+let addItem = document.getElementById("add-item")
+
+headerTitle.innerHTML = "<h3>Hello</h3>"
+
+mainHeader.style.borderBottom = "solid 3px #000"
+
+addItem.style.fontWeight= "bold"
+addItem.style.color = "green"
