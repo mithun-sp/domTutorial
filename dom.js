299b7208438b0ce0f23ced773c8143b850693478
Mithun S <mithunpillai007@gmail.com>
2023-10-06 13:04:55 +0530

    Added green color to 3rd item

--- dom.js
+++ dom.js
@@ -8,3 +8,10 @@ mainHeader.style.borderBottom = "solid 3px #000"
 
 addItem.style.fontWeight= "bold"
 addItem.style.color = "green"
+
+let items = document.getElementsByClassName("list-group-item")
+items[2].style.backgroundColor = "green"
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight = "bold"
+}
